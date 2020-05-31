7a3b28714f7fab5a73123e56a5f668265b6551ee
Šipaila Steponas <steponas.sipaila@mif.stud.vu.lt>
2020-05-31 23:24:23 +0300

    working on pushing data to HDF

--- PreparingForHive.py
+++ PreparingForHive.py
@@ -1,6 +1,8 @@
-
+#!/usr/bin/env python3
+#from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql.types import *
 from pyspark.sql import *
+from pyspark import SparkContext
 import re
 
 
@@ -11,18 +13,18 @@ print(csv_names)
 
 
 #creating list of headers and dictionary with key(unique_header):csv_data 
-#!!!  should clear these out when using sqlContext (contains invalid character(s) among " ,;{}()\n\t=")
+
 csv_headers = []
 csv_data = {}
 for item in csv_names:
 	csv_name = sc.textFile("/users/steponas/Documents/Projects/DataEnginiering/csvData/" + item)
 	try: temp = csv_name.map(lambda p: p.split(","))	
 	except:
-		print("couldnt format the" + str(csv_name))
+		print("couldn't format the" + str(csv_name))
 		continue
 	try: str(temp.first())	
 	except:
-		print("couldnt cast to string " + str(csv_name))
+		print("couldn't cast to string " + str(csv_name))
 		continue
 	header = temp.first()
 	if str(header) in csv_headers:
@@ -34,17 +36,18 @@ for item in csv_names:
 		csv_data[str(header)] = temp
 
 #making a list of dataframes for eatch unique key(unique_header):csv_data 
+#common error when casting to tables (contains invalid character(s) among " ,;{}()\n\t=")
 dfList = []
 for key in csv_data:
 	count = 0
-	header = re.sub("[][ ]", '', key).split(",")  
+	header = re.sub("[][ '']", '', key).split(",")  
 	header_row = Row(*header)
 	dfList.append(csv_data[key].map(lambda p: header_row(*p[:len(header)])).toDF())
 	count += 1
 
-#creating temporary table out of my dataframe and selecting all data from temp table to my actual table
-dfList[0].createOrReplaceTempView("mytempTable") 
-sqlContext.sql("create table mytable as select * from mytempTable");
+#prepareing data for sending to HDF
+#for frame in dfList:
+#	dfList[dfList.index(frame)].write.format("csv").save("/users/steponas/Documents/Projects/DataEnginiering/output")
 
 
 
